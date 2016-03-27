90ef6a57a767959cbb42691e0cf2bce82775f37d
Anonymous Candidate <anon@example.com>
2016-03-26 20:14:37 -0600

    Complete step 3.

--- src/anon_todo_2/handler.clj
+++ src/anon_todo_2/handler.clj
@@ -5,6 +5,7 @@
 
 (defroutes app-routes
   (GET "/" [] "<img src=\"http://www.zlok.net/blog/wp-content/uploads/under-construction-13.gif\" alt=\"A man hitting the words 'under construction' with a hammer\"></img>")
+  (GET "/about" [] "<p>This project is an anonymous todo list.  For democracy works!</p>")
   (route/not-found "Not Found"))
 
 (def app
