86f8818550dad03083b0d37d34798374fe139f29
Anonymous Candidate <anon@example.com>
2016-03-26 20:04:07 -0600

    Complete step 2.  Animated .gif added.

--- src/anon_todo_2/handler.clj
+++ src/anon_todo_2/handler.clj
@@ -4,7 +4,7 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "<img src=\"http://www.zlok.net/blog/wp-content/uploads/under-construction-13.gif\" alt=\"A man hitting the words 'under construction' with a hammer\"></img>")
   (route/not-found "Not Found"))
 
 (def app
