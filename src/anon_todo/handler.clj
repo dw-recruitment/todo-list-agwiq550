3df9c62a8e499193343d93bc0751d4689c9b1865
Anonymous Candidate <anon@example.com>
2016-04-02 18:31:11 -0600

    Complete step 9.

--- src/anon_todo/handler.clj
+++ src/anon_todo/handler.clj
@@ -8,16 +8,21 @@
             [hiccup.core :as hiccup]
             [hiccup.page :as hp]))
 
-;; so the db stuff goes to the model
-;; the hiccup / view stuff goes to the view
-;; the routing stuff goes to the controller
-
+;; -- MODEL
 
 (def sql-address "postgresql://localhost:5432/anon-todo")
 
 (defn db-migrate
   "Set up the db"
   []
+  (if (empty? (sql/query sql-address ["SELECT * FROM pg_class WHERE relname='listoflists'"]))
+    (sql/db-do-commands sql-address
+                        (sql/create-table-ddl :listoflists
+                                              [:id :serial "primary key"]
+                                              [:title :text])))
+  (if (empty? (sql/query sql-address ["SELECT * FROM listoflists WHERE title = 'Initial Todo List'"]))
+    (sql/insert! sql-address :listoflists
+                 {:title "Initial Todo List"}))
   (if (empty? (sql/query sql-address ["SELECT * FROM pg_class WHERE relname='todos'"]))
     (sql/db-do-commands sql-address
                         (sql/create-table-ddl :todos
@@ -25,7 +30,7 @@
                                               [:description :text]
                                               [:done :bool]
                                               [:name :text]
-                                              [:list :int])))
+                                              [:list :int "references listoflists"])))
   (if (empty? (sql/query sql-address ["SELECT * FROM todos WHERE name = 'anon'"]))
     (sql/insert! sql-address :todos
                  {:description "Find the flow" :done false :name "anon" :list 1}
@@ -34,89 +39,132 @@
 (db-migrate)
 
 (defn get-todos
+  [listid]
+  (sql/query sql-address ["SELECT * FROM todos where list = ? order by id asc" listid]))
+
+(defn get-listoflists
   []
-  (sql/query sql-address ["SELECT * FROM todos order by id asc"]))
+  (sql/query sql-address ["SELECT * FROM listoflists"]))
 
 (defn post-todo
   [todo]
-  (sql/insert! sql-address :todos {:description (:description todo) :done false}))
+  (sql/insert! sql-address :todos {:description (:description todo) :done false :list (Integer/parseInt (:list todo))}))
+
+(defn post-new-list
+  [title]
+  (sql/insert! sql-address :listoflists {:title title}))
 
 (defn update-todo
   [col new-val id]
-  (println new-val)
   (sql/update! sql-address :todos {col new-val} [ "id = ?" (Integer/parseInt id)]))
 
 (defn delete-todo
   [id]
   (sql/delete! sql-address :todos [ "id = ?" (Integer/parseInt id)]))
 
+(defn delete-list
+  [id]
+  (sql/delete! sql-address :listoflists [ "id = ?" (Integer/parseInt id)]))
+
 (defn toggle-todo
   [todo]
   (update-todo :done (not (read-string (:done todo))) (:id todo)))
 
+;; -- VIEW
+
 (defn layout-common
   [title & body]
   (hiccup/html
-    [:head
-      [:meta {:charset "utf-8"}]
-      [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge, chrome=1"}]
-      [:meta {:name "vieport" :content
-              "width=device-width, initial-scale=1, maximum-scale=1"}]
-      [:title title]
-      [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/sandstone/bootstrap.min.css"}]]
-    [:body
-     [:div.container body]]))
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge, chrome=1"}]
+    [:meta {:name "vieport" :content
+            "width=device-width, initial-scale=1, maximum-scale=1"}]
+    [:title title]
+    [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/sandstone/bootstrap.min.css"}]]
+   [:body
+    [:div.container body]]))
 
 (defn todo-desc
   [todo]
-  (if (:done todo)
-    [:del (:description todo)]
-    (:description todo)))
+  [:h2 (if (:done todo)
+         [:del (:description todo)]
+         (:description todo))])
 
 (defn todo-cluster
   [todo]
   (hiccup/html
-    [:div.row
-      [:li.list-group-item
-        (todo-desc todo)
-        [:div.input-group
-          [:div.input-group-btn
-            [:form {:action (str "/update-done/" (:id todo)) :method "POST"}
-              (anti-forgery-field)
-              [:input.btn.btn-info.btn-block {:type "submit" :value (if (:done todo)
-                                                                        "Undo"
-                                                                        "Complete")}]
-              [:input {:type "hidden" :name "done" :value (str (:done todo))}]
-              [:input {:type "hidden" :name "description" :value (str (:description todo))}]
-              [:input {:type "hidden" :name "list" :value (str (:list todo))}]
-              [:input {:type "hidden" :name "name" :value (str (:name todo))}]]
-            [:form {:action (str "/delete-todo/" (:id todo)) :method "POST"}
-              (anti-forgery-field)
-              [:input.btn.btn-danger.btn-block {:type "submit" :value "Delete this Todo"}]]]]]]))
+   [:li.list-group-item
+    (todo-desc todo)
+    [:div.input-group
+     [:div.input-group-btn
+      [:form {:action (str "/update-done/" (:id todo)) :method "POST"}
+       (anti-forgery-field)
+       [:input.btn.btn-info.btn-sm {:type "submit" :value (if (:done todo)
+                                                            "Undo"
+                                                            "Complete")}]
+       [:input {:type "hidden" :name "done" :value (str (:done todo))}]
+       [:input {:type "hidden" :name "description" :value (str (:description todo))}]
+       [:input {:type "hidden" :name "list" :value (str (:list todo))}]
+       [:input {:type "hidden" :name "name" :value (str (:name todo))}]]
+      [:form {:action (str "/delete-todo/" (:id todo)) :method "POST"}
+       (anti-forgery-field)
+       [:input.btn.btn-danger.btn-sm {:type "submit" :value "Delete this Todo"}]]]]]))
 
 ;; almost got all the view out of the routes.  That's next.
 
 (defn todo-list
+  [list-info]
+  (hiccup/html
+   [:div.col-md-4
+    [:div.panel.panel-default
+     [:div.panel-heading (str (:title list-info))
+      [:a.btn.btn-danger.btn-sm.pull-right {:href (str "/delete-list/" (:id list-info)) :style (str "visibility:" (if (empty? (get-todos (:id list-info)))
+                                                                                       "visible"
+                                                                                       "hidden"))}
+      [:span.glyphicon.glyphicon-minus]]]
+     [:div.panel-body
+      [:div.row
+       [:ul.list-group
+        (map todo-cluster (get-todos (:id list-info)))]
+       [:form {:action "/add-todo" :method "POST"}
+        (anti-forgery-field)
+        [:div.input-group
+         [:input {:type "hidden" :name "list" :value (:id list-info)}]
+         [:input.form-control {:type "Text" :name "description" :placeholder "Todo: "}]
+         [:span.input-group-btn
+          [:input.btn.btn-success {:type "submit" :value "Add todo to list"}]]]]]]]]))
+
+(defn list-add-form
   []
   (hiccup/html
-    [:div.col-md-4
-     [:ul.list-group
-      (map todo-cluster (get-todos))]
-     [:div.input-group
-      [:form.form-control {:action "/add-todo" :method "POST"}
-        (anti-forgery-field)
-        [:input {:type "Text" :name "description" :placeholder "Todo: "}]
-       [:span.input-group-btn
-        [:input.btn.btn-success {:type "submit" :value "Add todo to list"}]]]]]))
+   [:form {:action (str "/add-list") :method "POST"}
+    (anti-forgery-field)
+    [:div.input-group
+     [:input.form-control {:type "Text" :name "title" :placeholder "New List Title"}]
+     [:span.input-group-btn
+      [:input.btn.btn-success {:type "submit" :value "Add new list"}]]]]))
 
+(defn main-page
+  []
+  [:div (map todo-list (get-listoflists))
+   (list-add-form)])
+
+;; -- CONTROLLER
 
 (defroutes app-routes
   (GET "/" []
-       (layout-common "Todo Lists: Anonymous" (todo-list)))
+       (layout-common "Todo Lists: Anonymous" (main-page)))
   (GET "/about" []
-      "<p>This project is an anonymous todo list.  For democracy works!</p>")
+       "<p>This project is an anonymous todo list.  For democracy works!</p>")
   (POST "/add-todo" [_ & rest]
-        (post-todo rest)
+        (if (empty? (:description rest))
+          (post-todo (assoc-in rest [:description] "Fill in the todo field before adding a todo"))
+          (post-todo rest))
+        (redirect "/"))
+  (POST "/add-list" req
+        ;; (println (:params req))
+        (post-new-list (:title (:params req)))
         (redirect "/"))
   (POST "/update-done/:id" [id & rest]
         (toggle-todo (assoc-in rest [:id] id))
@@ -124,10 +172,10 @@
   (POST "/delete-todo/:id" [id & rest]
         (delete-todo id)
         (redirect "/"))
+  (GET "/delete-list/:id" [id & rest]
+        (delete-list id)
+        (redirect "/"))
   (route/not-found "Not Found"))
-
-;; (fn [e] (println e)
-;;   (update-done todo))
 
 (def app
   (wrap-defaults app-routes site-defaults))
